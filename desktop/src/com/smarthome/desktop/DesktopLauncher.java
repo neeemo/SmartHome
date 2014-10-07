b4365e337d38f8b42b2bc0111b20d624da733274
Steven Teng <teng.steven@gmail.com>
2014-10-07 13:11:58 +0200

    initial commit
    Just project start

---
+++ desktop/src/com/smarthome/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.smarthome.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.smarthome.SmartHome;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SmartHome(), config);
+	}
+}
